14860ddad47b03378e10c170cfb1166657a8525a
변재호 <bjho606@naver.com>
2022-09-20 18:03:48 +0900

    1062, 15684

--- 스터디 문제/week14/jb_1062.py
+++ 스터디 문제/week14/jb_1062.py
@@ -1,44 +1,70 @@
+from itertools import combinations
 import sys
 
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
+alphabets = set(chr(i) for i in range(97, 123))
 K -= 5      # a, n, t, i, c
-default_alphabet = ['a', 'n', 't', 'i', 'c']
+default_alphabet = {'a', 'n', 't', 'i', 'c'}
+alphabets -= default_alphabet
+# print(alphabets)
+
+words = []
+
+# 문자 가공
+for i in range(N):
+    origin_word = input()
+    final_word = set(origin_word.rstrip()[4:-4]) - default_alphabet
+
+    words.append(final_word)
+# print(words)
+
+learned_alphabet = [0] * 26
+for i in default_alphabet:
+    learned_alphabet[ord(i)-97] = 1
+# print(learned_alphabet)
 
 # 예외 조건
 if K < 0:
     print(0)
     exit()
 
-words = []
-alphabet_used = [0] * 26
-# print(ord('a'))   # a = 97
-for used in default_alphabet:
-    alphabet_used[ord(used) - 97] = 1
-# print(alphabet_used)
+ans = 0
 
-# def check_updates(learned_alphabet, words):
+for combi_alpha in combinations(alphabets, K):
+    # 조합의 단어 1로 표시
+    for ca in combi_alpha:
+        # print(ca)
+        learned_alphabet[ord(ca)-97] = 1
 
+    count = 0
+    for word in words:
+        # print(word)
+        temp = 0
+        for w in word:
+            if learned_alphabet[ord(w)-97] == 0:
+                temp = 1
+                break
+        if temp == 0:
+            count += 1
 
-for i in range(N):
-    origin_word = input()
-    final_word = ''     # 최종 가공된 글자
-    # anta, tica 제거
-    trimmed_word = origin_word[4:-5]
-    # print(trimmed_word)
-    for alphas in trimmed_word:
-        # a, n, t, i, c 포함하는 글자 제거
-        if alphas in default_alphabet:
-            continue
-        # 중복되는 글자 제거
-        if alphas in final_word:
-            continue
+    ans = max(ans, count)
 
-        final_word += alphas
-        # print(final_word)
+    # 조합의 단어 다시 0으로 초기화
+    for ca in combi_alpha:
+        learned_alphabet[ord(ca)-97] = 0
 
-    words.append(final_word)
-print(words)
+# # --- 시간 초과
+# for combi_alpha in combinations(alphabets, K):
+#     # print(combi_alpha)
+#     count = 0
+#     for word in words:
+#         # temp = len(word)
+#         # print(word, temp)
+#         if not word - set(combi_alpha):
+#             count += 1
 
-# for word in words:
+#     ans = max(ans, count)
+
+print(ans)
--- 스터디 문제/week14/jb_15684.py
+++ 스터디 문제/week14/jb_15684.py
@@ -1,8 +1,26 @@
+# ㅠㅠㅠㅠ
 import sys
 
 input = sys.stdin.readline
 
+# 한 세로선마다 짝수개의 통로가 있어야함
+# 짝수개의 통로 사이에 다른 선이 끼어들면 안됨
+
 N, M, H = map(int, input().split())
+
+board = [[0] * (N) for _ in range(H+1)]
+# print(board)
+
+# 그림으로 체크
+def check_board():
+    for i in range(1, H+1):
+        for j in range(1, N):
+            print(board[i][j], end=' ')
+        print()
 
 for i in range(M):
     a, b = map(int, input().split())
+    board[a][b] = 1
+
+# check_board()
+
